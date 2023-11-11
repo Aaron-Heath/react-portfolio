056ffc775a8ee6d46779d3c57c813f9a78b05bf5
aheath <aaron.heath5447@gmail.com>
2023-11-10 23:04:55 -0500

    Create Link component. Add contact me button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Button, Navbar } from 'react-bootstrap';
 import Header from './Components/Header';
 import Name from './Components/Name'
+import Link from './Components/Link';
 
 let App = () => {
   return (
@@ -13,6 +14,11 @@ let App = () => {
         <div class="col-md-8">
           <div class="col-md-6">
             <Name inline={false} />
+            <div id="socials">
+              <Link href={"https://github.com/Aaron-Heath"} text="Github"/>  |  <Link href="https://www.linkedin.com/in/aaron-heath-2b0a5510b/" text="LinkedIn"/>
+
+            </div>
+            <Button id="contact-btn" variant="contact">CONTACT ME</Button>
           </div>
           <div class="col-md-6">
             
