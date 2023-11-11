94fb5015a18503adff5bea62fdf03c9ef280f66d
aheath <aaron.heath5447@gmail.com>
2023-11-10 22:29:39 -0500

    Add styling and additional element to name component

--- src/Components/Name.js
+++ src/Components/Name.js
@@ -3,17 +3,27 @@ import React from 'react'
 export default function Name({inline=true}) {
     if(inline === true) {
         return (
+            <>
             <div class="branded-name">
                 <p class="mint">Aaron Heath<span class="accent">.</span></p>
             </div>
+            <div class="dash">
+
+            </div>
+            </>
+            
           )
     }
     return (
+        <>
         <div class="branded-name">
             <p class="mint">Aaron</p>
             <p class="mint">Heath<span class="accent">.</span></p>
         </div>
-    
+        <div class="dash">
+            
+        </div>
+        </>
     )
 
 }
