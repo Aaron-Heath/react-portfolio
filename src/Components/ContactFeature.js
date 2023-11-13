d0597bbb8f1459cd0f3993ec7cbc977080c4b4fc
aheath <aaron.heath5447@gmail.com>
2023-11-13 18:44:08 -0500

    Add contactFeature content.

--- src/Components/ContactFeature.js
+++ src/Components/ContactFeature.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import ContactCard from './ContactCard'
+import { Button, Form } from 'react-bootstrap'
+import DownloadIcon from '../icons/icons8-download-52.png'
 
 export default function ContactFeature() {
   return (
     <div id="contact-section" class="row">
         <div class="row text-centered">
-            <h2>CONTACT ME</h2>
+            <h3>Contact Me</h3>
         </div>
         <div class="row">
-            <section id="contact-info" class="col-md-6">
-              <ContactCard type="email" text="Email" href="mailto:aaron.heath5447@gmail.com"/>
-              <ContactCard type="linkedin" text="LinkedIn" href="https://www.linkedin.com/in/aaron-heath-2b0a5510b/"/>
+            <section id="contact-email" class="col-md-6">
+              <Form>
+                <Form.Group className="mb-3" controlId="form-email">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="form-subject">
+                  <Form.Label>Subject</Form.Label>
+                  <Form.Control type="text" placeholder="Subject" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="form-message">
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control as="textarea" rows={4} placeholder="Your message..." />
+                </Form.Group>
+              </Form>
             </section>
-            <section id="contact-form" class="col-md-6">
-
+            <section id="contact-info" class="col-md-6">
+            <ContactCard type="email" text="Email" href="mailto:aaron.heath5447@gmail.com"/>
+              <ContactCard type="linkedin" text="LinkedIn" href="https://www.linkedin.com/in/aaron-heath-2b0a5510b/"/>
+              <div class="left-align">
+                <Button id="resume-btn" variant="resume"><img src={DownloadIcon} alt="Download Icon"/>  Resume</Button>
+              </div>
             </section>
         </div>
         
