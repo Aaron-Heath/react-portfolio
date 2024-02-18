eb8063aea8e929e926558d0c63f2e295a49c764c
aheath <aaron.heath5447@gmail.com>
2024-02-18 18:48:58 -0500

    Add github and linkedin icons for footer. Add copyright and link to source code

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import githubLogo from '../../icons/icons8-github-50.png'
+import linkedinLogo from '../../icons/icons8-linkedin-60.png'
+import './footer.css';
+
 export default function Footer() {
     const styles = {
         footerStyle: {
@@ -20,13 +24,17 @@ export default function Footer() {
         rowStyle: {
             // border: "2px red solid",
             minWidth: "100%"
-        }
+        },
     }
 
   return (
     <footer id="footer" style={styles.footerStyle}>
-        <div className='d-flex justify-content-between' style={styles.containerStyle}>
-            This is text
+        <div className='d-flex justify-content-between align-items-center' style={styles.containerStyle}>
+        <span>©2024 Aaron Heath. <a href="https://github.com/Aaron-Heath/react-portfolio" rel="noopener noreferrer" target='_blank'><u>View the code</u></a>.</span>
+            <div className='footer-icons'>
+                <a href="https://github.com/Aaron-Heath" rel="noopener noreferrer" target='_blank'><img width="25" height="25" src={githubLogo} alt="github"/></a>
+                <a href="https://www.linkedin.com/in/aaron-heath-2b0a5510b/" rel="noopener noreferrer" target='_blank'><img width="25" height="25" src={linkedinLogo} alt="github"/></a>
+            </div>
         </div>
     </footer>
   )
