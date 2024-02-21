e71e46c05046277d6f5d2d07ee8b59c524d3673a
aheath <aaron.heath5447@gmail.com>
2024-02-20 19:14:34 -0500

    refactoring header to debug netlify deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header';
 import BioFeature from './components/bioFeature';
 import ContactFeature from './components/ContactFeature';
 import AboutMe from './about/AboutMe';
