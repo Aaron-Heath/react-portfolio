c0932952f2248cdab063e8828fe1775cab7ae548
aheath <aaron.heath5447@gmail.com>
2024-02-20 21:43:31 -0500

    Debug vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    base: "/react-portfolio",
-    plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  }
 });
