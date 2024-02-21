af3e3b545ed1ccefce5ce63fa4e1276562573b9f
aheath <aaron.heath5447@gmail.com>
2024-02-20 20:28:51 -0500

    change output directory to default

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    build: {
-      outDir: 'build',
-    },
     base: "/react-portfolio",
     plugins: [react()],
   };
