36dca9045204ee78c631d892ef4a1f87e04aaab5
aheath <aaron.heath5447@gmail.com>
2024-02-27 18:32:09 -0500

    Install react-portfolio. Implement page changes for each path

--- src/App.jsx
+++ src/App.jsx
@@ -2,61 +2,68 @@ import React, { useState } from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import './App.css';
 import Header from './components/Header';
-import BioFeature from './components/bioFeature';
-import ContactFeature from './components/contact/ContactFeature';
-import AboutMe from './about/AboutMe';
-import ProjectsFeature from './components/projects/ProjectFeature';
-import Footer from './components/Footer';
+import Footer from './components/footer'
+import { Outlet } from 'react-router-dom';
 
 
 let App = () => {
   const [page, setPage] = useState("home");
 
+  return(
+    <>
+      <Header setPage={setPage}/>
+      <main className='container'>
+        <Outlet/>
+      </main>
+      <Footer/>
+    </>
+  )
+
   // Switch case determines which main page or "feature" should render.
   // Child elements that can change this state should be passed the "setPage" function
-  switch(page){
-    case "contact":return (
-      <>
-      <Header setPage={setPage}></Header>
-      <main className="container">
-        <ContactFeature/>
-      </main>
-      <Footer></Footer>
-      </>
-    );
+  // switch(page){
+  //   case "contact":return (
+  //     <>
+  //     <Header setPage={setPage}></Header>
+  //     <main className="container">
+  //       <ContactFeature/>
+  //     </main>
+  //     <Footer></Footer>
+  //     </>
+  //   );
 
-    case "about": return (
-      <>
-        <Header setPage={setPage}></Header>
-        <main className="container">
-          <AboutMe/>
-        </main>
-        <Footer></Footer>
-      </>
-    );
+  //   case "about": return (
+  //     <>
+  //       <Header setPage={setPage}></Header>
+  //       <main className="container">
+  //         <AboutMe/>
+  //       </main>
+  //       <Footer></Footer>
+  //     </>
+  //   );
 
-    case "projects": return (
-      <>
-      <Header setPage={setPage}></Header>
-      <main className="container">
-        <ProjectsFeature/>
-      </main>
-      <Footer></Footer>
-      </>
-    )
+  //   case "projects": return (
+  //     <>
+  //     <Header setPage={setPage}></Header>
+  //     <main className="container">
+  //       <ProjectsFeature/>
+  //     </main>
+  //     <Footer></Footer>
+  //     </>
+  //   )
 
-    default: return (
-      <>
-      <Header setPage={setPage}></Header>
-      <main className="container">
-        <BioFeature/>
-        {/* <AboutMe/>
-        <ProjectsFeature></ProjectsFeature>
-        <ContactFeature/> */}
-      </main>
-      <Footer></Footer>
-      </>);
-  }
+  //   default: return (
+  //     <>
+  //     <Header setPage={setPage}></Header>
+  //     <main className="container">
+  //       <BioFeature/>
+  //       {/* <AboutMe/>
+  //       <ProjectsFeature></ProjectsFeature>
+  //       <ContactFeature/> */}
+  //     </main>
+  //     <Footer></Footer>
+  //     </>);
+  // }
   
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,56 @@ import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals.js';
 
+import BioFeature from './components/bioFeature/index.jsx'
+import ContactFeature from './components/contact/ContactFeature'
+import AboutMe from './about/AboutMe.jsx'
+import ProjectFeature from './components/projects/ProjectFeature'
+
+
 /**
  * React Boostrap Config
  */
 import "react-bootstrap/dist/react-bootstrap.js"
-// import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import './App.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <App/>,
+      children: [
+          {
+              index: true,
+              element: <BioFeature/>
+          },
+          {
+            path: '/contact',
+            element: <ContactFeature/>
+          },
+          {
+            path: '/about',
+            element: <AboutMe/>
+          },
+          {
+            path: '/projects',
+            element: <ProjectFeature/>
+          }
+
+      ]
+  }
+]);
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
