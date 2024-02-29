e4f397f9b72e38af9daf78be0f011f6c946bb67b
aheath <aaron.heath5447@gmail.com>
2024-02-29 18:31:32 -0500

    Add functionality to home page Learn more and contact me buttons

--- src/Components/bioFeature/index.jsx
+++ src/Components/bioFeature/index.jsx
@@ -11,6 +11,19 @@ export default function BioFeature() {
     let featureTitle = "Full Stack Web Developer, Software Engineer"
     let featureText = "My path to web development and software engineering began with 12 hour nights at a 911 Center in Montgomery County, Pennsylvania..."
     let featureId = "bio-feature"
+
+    function handleClick(event) {
+      console.log(event.target)
+      if(!event.target.matches("button")) {
+        return;
+      }
+
+      if(event.target.matches("#contact-btn")) {
+        window.location="/contact"
+      }
+      
+    }
+    
   return (
     <div className="row">
         <div className="row col-md-8">
@@ -20,7 +33,7 @@ export default function BioFeature() {
               <Link href={"https://github.com/Aaron-Heath"} text="Github"/>  |  <Link href="https://www.linkedin.com/in/aaron-heath-2b0a5510b/" text="LinkedIn"/>
 
             </div>
-            <Button id="contact-btn" variant="contact">CONTACT ME</Button>
+            <Button id="contact-btn" variant="contact" onClick={handleClick}>CONTACT ME</Button>
           </div>
           <div className="col-md-6" id="bio-pic-container">
             <img alt="Aaron Heath Portfolio Picture" src={BioPic} id="bio-pic"></img>
--- src/components/FeatureCard/index.jsx
+++ src/components/FeatureCard/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
 export default function FeatureCard({header,title,text, link, id}) {
+
+  function handleClick(event) {
+    if(!event.target.matches("#learn-more")) {
+      return;
+    }
+
+    window.location="/about"
+  }
+
+
   return (
     <div className="feature-card" id={id}>
       <h2>{header}</h2>
       <h3>{title}</h3>
       <p>{text}</p>
-      <span>Learn more</span>
+      <span id="learn-more" onClick={handleClick}>Learn more</span>
     </div>
   )
 }
