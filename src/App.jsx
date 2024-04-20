ccd0c670952a596266cd691eabef205ff8346185
aheath <aaron.heath5447@gmail.com>
2024-04-20 12:26:08 -0400

    add live link to plantpoppa

--- src/App.jsx
+++ src/App.jsx
@@ -17,54 +17,7 @@ let App = () => {
       </main>
       <Footer/>
     </>
-  )
-
-  // Switch case determines which main page or "feature" should render.
-  // Child elements that can change this state should be passed the "setPage" function
-  // switch(page){
-  //   case "contact":return (
-  //     <>
-  //     <Header setPage={setPage}></Header>
-  //     <main className="container">
-  //       <ContactFeature/>
-  //     </main>
-  //     <Footer></Footer>
-  //     </>
-  //   );
-
-  //   case "about": return (
-  //     <>
-  //       <Header setPage={setPage}></Header>
-  //       <main className="container">
-  //         <AboutMe/>
-  //       </main>
-  //       <Footer></Footer>
-  //     </>
-  //   );
-
-  //   case "projects": return (
-  //     <>
-  //     <Header setPage={setPage}></Header>
-  //     <main className="container">
-  //       <ProjectsFeature/>
-  //     </main>
-  //     <Footer></Footer>
-  //     </>
-  //   )
-
-  //   default: return (
-  //     <>
-  //     <Header setPage={setPage}></Header>
-  //     <main className="container">
-  //       <BioFeature/>
-  //       {/* <AboutMe/>
-  //       <ProjectsFeature></ProjectsFeature>
-  //       <ContactFeature/> */}
-  //     </main>
-  //     <Footer></Footer>
-  //     </>);
-  // }
-  
+  ) 
 }
 
 export default App;
